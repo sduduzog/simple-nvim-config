29a26a6158421422fde7f81483f415e951f50870
Beautus <gumedesduduzo@gmail.com>
2023-08-28 11:06:22 +0200

    customize start page buttons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,7 +3,22 @@ return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require 'alpha'.setup(require 'alpha.themes.theta'.config)
+      local buttons = {
+        type = 'group',
+        val = {
+          { type = 'text', val = 'Quick links', opts = { hl = 'SpecialComment', position = 'center' } },
+        }
+      }
+      local theta = require('alpha.themes.dashboard')
+      theta.section.buttons.val = {
+        theta.button('e', '  New file', '<cmd>ene<CR>'),
+        theta.button('<leader> s f', '󰈞  Find file'),
+        theta.button('<leader> s g', '󰊄  Live grep'),
+        theta.button('c', '  Configuration', '<cmd>tabnew $MYVIMRC <bar> tcd %:h<cr>'),
+        theta.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
+        theta.button('q', '󰅚  Quit', '<cmd>qa<CR>'),
+      }
+      require 'alpha'.setup(theta.opts)
     end
   }
 }
