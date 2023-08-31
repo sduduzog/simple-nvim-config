deb66eafb3e25bc350fe629abc0f8fbb769cecb2
Beautus <gumedesduduzo@gmail.com>
2023-08-31 11:38:59 +0200

    remove everything

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
+function platform()
+  return package.config:sub(1, 1) == '\\' and 'win' or 'unix'
+end
+
+if platform() == 'win' then
+  local powershell_options = {
+    shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+    shellcmdflag =
+    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+    shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+    shellquote = '',
+    shellxquote = '',
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -66,4 +86,60 @@ vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete buffer' })
 
 require('lazy').setup('plugins');
 
+pcall(require, 'options')
+
 vim.cmd.colorscheme 'catppuccin-frappe'
+
+
+function split(pString, pPattern)
+  local Table = {} -- NOTE: use {n = 0} in Lua-5.0
+  local fpat = '(.-)' .. pPattern
+  local last_end = 1
+  local s, e, cap = pString:find(fpat, 1)
+  while s do
+    if s ~= 1 or cap ~= '' then
+      table.insert(Table, cap)
+    end
+    last_end = e + 1
+    s, e, cap = pString:find(fpat, last_end)
+  end
+  if last_end <= #pString then
+    cap = pString:sub(last_end)
+    table.insert(Table, cap)
+  end
+  return Table
+end
+
+function clean_path(filename)
+  local pathseperator = package.config:sub(1, 1)
+  return table.concat(split(filename, '/'), pathseperator)
+end
+
+vim.api.nvim_create_user_command('CreateNewFileFromPrompt', function()
+    local current_filename = vim.api.nvim_buf_get_name(0)
+    if current_filename ~= '' then
+      print('buffer is associated with an existing file')
+      return
+    end
+    vim.ui.input({ prompt = 'filename: ' }, function(filename)
+      vim.cmd('redraw')
+      print('\n') -- pretty much resetting the status line
+      local file_exists = io.open(filename, 'r')
+      if file_exists ~= nil then
+        io.close(file_exists)
+        print(string.format('%s already exists', filename))
+        return
+      end
+
+
+      local dir = string.match(filename, '(.+)/[^/]*$')
+
+      if dir then
+        os.execute('mkdir ' .. clean_path(dir))
+      end
+      vim.cmd(':write ' .. filename)
+    end)
+  end,
+  {})
+
+vim.keymap.set('n', '<C-s>', '<Cmd>CreateNewFileFromPrompt<CR>')
